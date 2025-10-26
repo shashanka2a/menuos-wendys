87c6ae8a536d7af4903df143985c674080c249ff
shashank <jagannathamshashank@gmail.com>
2025-10-26 19:29:26 -0400

    Fix image loading: Add external domains to Next.js config
    - Add Unsplash, Wendy's, and other external domains to remotePatterns
    - Enable proper loading of external food images
    - Fix placeholder image issue by allowing external image sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,46 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wendys.bs',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.wendys.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wendys.ky',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'people.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.tegna-media.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
